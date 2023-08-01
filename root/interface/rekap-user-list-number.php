0c3505e2b99311655b3d14637a71a6971c045a24
Yoko <40120kyoko@gmail.com>
2023-08-01 16:26:32 +0700

    pagianasi admin->rekap user (pagination)

--- root/interface/rekap-user-list-number.php
+++ root/interface/rekap-user-list-number.php
@@ -116,13 +116,77 @@
 	
 	$jumlahPage = ceil($c/$limit);
 	$startFrom = 0;
-	
-	for($i=1; $i<=$jumlahPage; $i++){
-?>
-	<a id="number<?php echo $i; ?>" class="item <?php if($i==1){ echo "active"; }?>" onclick="updateList('<?php echo $startFrom; ?>','<?php echo $i; ?>')">
-		<?php echo $i ?>
-	</a>
-<?php
+	if($jumlahPage<=3){
+     // 123  
+     for($i=1; $i<=$jumlahPage; $i++){
+        ?>
+            <a id="number<?php echo $i; ?>" class="item <?php if($i==1){ echo "active"; }?>" onclick="updateListRekapUser('<?php echo $startFrom; ?>','<?php echo $i; ?>')">
+                <?php echo $i ?>
+            </a>
+        <?php
 		$startFrom = $startFrom+$limit;
-	}
+     }
+    }else{
+     //123>   
+     for($i=1; $i<=3; $i++){
+        ?>
+            <a id="number<?php echo $i; ?>" class="item <?php if($i==1){ echo "active"; }?>" onclick="updateListRekapUser('<?php echo $startFrom; ?>','<?php echo $i; ?>')">
+                <?php echo $i ?>
+            </a>
+        <?php
+		$startFrom = $startFrom+$limit;
+     }
+     ?>
+     <a class="item" onclick="updateListRekapUser('<?php echo $limit; ?>','2')">
+         Next
+     </a>
+     <?php
+    }
 ?>
+
+<script>
+    function updateListRekapUser(start, id){
+    var jumlahPage = <?php echo $jumlahPage ?>;
+    var limitPage = <?php echo $limit ?>;
+    var startFrom = 0;
+    var tempMultiplier;
+    var tempPageNum;
+    id = Number(id);
+    console.log(jumlahPage);
+    $('#lastPage').val(start);
+    $("#pageNumber a").remove();
+    // 123 , 123> , <234>, <345
+    if(jumlahPage<=3){
+        for(var i=1; i<=jumlahPage; i++){
+            $("#pageNumber").append("<a id=\"number"+i+"\" class=\"item\" onclick=\"updateListRekapUser('"+startFrom+"','"+i+"')\">"+i+"</a>");
+            startFrom = startFrom+limitPage;
+        }   
+    }else{
+        if(id>2){
+            tempMultiplier = id-2;
+            tempPageNum = id-1;
+            $("#pageNumber").append("<a class=\"item\" onclick=\"updateListRekapUser('"+(limitPage*(tempMultiplier))+"','"+(tempPageNum)+"')\">Prev</a>");
+        }
+        if(id==jumlahPage){
+            tempMultiplier = id-3;
+            tempPageNum = id-2;
+            $("#pageNumber").append("<a id=\"number"+(id-2)+"\" class=\"item\" onclick=\"updateListRekapUser('"+(limitPage*(tempMultiplier))+"','"+(tempPageNum)+"')\">"+(tempPageNum)+"</a>");
+        }
+        tempMultiplier = id-2;
+        tempPageNum = id-1;
+        $("#pageNumber").append("<a id=\"number"+(id-1)+"\" class=\"item\" onclick=\"updateListRekapUser('"+(limitPage*(tempMultiplier))+"','"+(tempPageNum)+"')\">"+(tempPageNum)+"</a>");
+        tempMultiplier = id-1;
+        $("#pageNumber").append("<a id=\"number"+(id)+"\" class=\"item\" onclick=\"updateListRekapUser('"+(limitPage*(tempMultiplier))+"','"+(id)+"')\">"+(id)+"</a>");
+        if(id!=jumlahPage){
+            tempPageNum = id+1;
+            $("#pageNumber").append("<a id=\"number"+(id+1)+"\" class=\"item\" onclick=\"updateListRekapUser('"+(limitPage*(id))+"','"+(tempPageNum)+"')\">"+(tempPageNum)+"</a>");
+        }
+        if(id<=(jumlahPage-2)){
+            tempPageNum = id+1;
+            $("#pageNumber").append("<a class=\"item\" onclick=\"updateListRekapUser('"+(limitPage*(id))+"','"+(tempPageNum)+"')\">Next</a>");
+        }
+    }
+    $("#number"+id).addClass("active");
+    dataList();
+}
+</script>
